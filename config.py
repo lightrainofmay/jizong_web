3435fd20cfc100a55127b0357b29c0d584e38022
lightrainofmay <167319364+lightrainofmay@users.noreply.github.com>
2025-06-10 16:35:16 +0800

    change

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
+LANGUAGES = ["中文", "English"]
+
 MENU = {
-    "首页 / Home": "home",
-    "菌种识别 / Mushroom ID": "identify",
-    "生态与保护 / Ecology": "ecology",
-    "烹饪教学 / Cooking": "cooking",
-    "文化故事 / Culture": "culture",
-    "社区互动 / Community": "community",
-    "菌类知识 / Mushroom Info": "mushroom_info",
-    "教学视频 / Teaching Video": "video"  
+    "中文": [
+        "首页",
+        "菌种识别",
+        "生态与保护",
+        "文化与故事",
+        "菌类知识",
+        "教学视频"
+    ],
+    "English": [
+        "Home",
+        "Mushroom Identification",
+        "Ecology & Conservation",
+        "Culture & Stories",
+        "Mushroom Knowledge",
+        "Teaching Videos"
+    ]
 }
